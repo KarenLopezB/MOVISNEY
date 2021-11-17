15155900ee39414286cf38b5b32e0ca4d3ca4c5f
KarenLopezB <53828090+KarenLopezB@users.noreply.github.com>
2021-11-16 17:26:31 -0700

    Add files via upload

---
+++ MOVISNEY/Pelicula.swift
@@ -0,0 +1,15 @@
+//
+//  Pelicula.swift
+//  MOVISNEY
+//
+//  Created by Alumno on 11/16/21.
+//  Copyright © 2021 Alumno. All rights reserved.
+//
+
+import Foundation
+
+class Pelicula {
+    var nombre = ""
+    var año = ""
+    var duracion = ""
+}
