0aa7ae898af767b14d3b774a1130e17ab74d4e68
KarenLopezB <53828090+KarenLopezB@users.noreply.github.com>
2021-11-22 17:21:31 -0700

    Add files via upload

---
+++ MOVISNEY/MOVISNEY/DetallesPeliculaController.swift
@@ -0,0 +1,41 @@
+//
+//  DetallesPeliculaController.swift
+//  MOVISNEY
+//
+//  Created by Alumno on 11/22/21.
+//  Copyright © 2021 Alumno. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DetallesPeliculaController : UIViewController {
+    
+    var pelicula : Pelicula = Pelicula(nombre: "", año: "", duracion: "", calificacion: "", directores: "", descripcion: "", estreno: "", reparto: "", cancion: "", vestido: "")
+    
+    @IBOutlet weak var lblNombre1: UILabel!
+    @IBOutlet weak var lblAno1: UILabel!
+    @IBOutlet weak var lblDuracion1: UILabel!
+    @IBOutlet weak var lblCalificacion1: UILabel!
+    @IBOutlet weak var lblDirectores1: UILabel!
+    @IBOutlet weak var lblDescripcion: UILabel!
+    @IBOutlet weak var lblEstreno: UILabel!
+    @IBOutlet weak var lblReparto: UILabel!
+    @IBOutlet weak var lblCancion: UILabel!
+    @IBOutlet weak var lblVestido: UILabel!
+    
+    override func viewDidLoad() {
+        self.title = pelicula.nombre
+        
+        lblNombre1.text = pelicula.nombre
+        lblAno1.text = pelicula.año
+        lblDuracion1.text = pelicula.duracion
+        lblCalificacion1.text = pelicula.calificacion
+        lblDirectores1.text = pelicula.directores
+        lblDescripcion.text = pelicula.descripcion
+        lblEstreno.text = pelicula.estreno
+        lblReparto.text = pelicula.reparto
+        lblCancion.text = pelicula.cancion
+        lblVestido.text = pelicula.vestido
+    }
+}
