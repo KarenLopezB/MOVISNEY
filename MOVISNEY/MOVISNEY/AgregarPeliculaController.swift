9f35d49e6a478c443a3384dba1c82897795afec3
KarenLopezB <53828090+KarenLopezB@users.noreply.github.com>
2021-11-23 16:58:45 -0700

    Add files via upload

---
+++ MOVISNEY/MOVISNEY/AgregarPeliculaController.swift
@@ -0,0 +1,28 @@
+//
+//  AgregarPeliculaController.swift
+//  MOVISNEY
+//
+//  Created by Alumno on 11/23/21.
+//  Copyright © 2021 Alumno. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AgregarPeliculaController : UIViewController {
+    
+    @IBOutlet weak var txtNombre: UITextField!
+    @IBOutlet weak var txtAno: UITextField!
+    @IBOutlet weak var txtDuracion: UITextField!
+    @IBOutlet weak var txtCalficacion: UITextField!
+    @IBOutlet weak var txtDirectores: UITextField!
+    @IBOutlet weak var txtDescripcion: UITextField!
+    @IBOutlet weak var txtEstreno: UITextField!
+    @IBOutlet weak var txtReparto: UITextField!
+    @IBOutlet weak var txtCancion: UITextField!
+    @IBOutlet weak var txtVestido: UITextField!
+    
+    override func viewDidLoad() {
+        
+    }
+}
